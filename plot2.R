dd7515071c4910309b74eb854788e449b9d7f0ad
J. Svehag <nattsuddare@gmail.com>
2016-01-23 06:22:49 +0100

    plot 2

---
+++ plot2.R
@@ -0,0 +1,22 @@
+args = commandArgs(trailingOnly=TRUE)
+if(length(args)>0 & args[1] == "debug"){
+  dat <- read.csv("household.csv")
+} else {
+  filename <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+  temp <- tempfile()
+  download.file(filename, temp)
+  header <- read.table(unz(temp, "household_power_consumption.txt"), nrows=1, sep=";")
+  dat <- read.table(unz(temp, "household_power_consumption.txt"), nrows=2880, skip=66637, sep=";")
+  colnames(dat) <- unlist(header)
+  unlink(temp)
+}
+
+png("plot2.png", bg="transparent")
+par(mar=c(5,4,4,2))
+plot(
+  strptime(paste(dat$Date, dat$Time), format="%d/%m/%Y %H:%M:%S"),
+  dat$Global_active_power,
+  type="l",
+  xlab="",
+  ylab="Global Active Power (kilowatts)")
+dev.off()
